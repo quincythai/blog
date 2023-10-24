159d45b619bf289c6420bfcb528901ff2ab78e07
Quincy Thai <quincythai50@icloud.com>
2023-10-23 20:37:18 -0700

    Save progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// TODO: connect this to the pages
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/about",
+    element: <div>About</div>,
+  },
+]);
 
 import Home from './components/Home';
 import About from './components/About';
@@ -10,14 +25,7 @@ import Contact from './components/Contact';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact render={() => <Home />} />
-        <Route path="/about" render={() => <About />} />
-        <Route path="/blog" render={() => <Blog />} />
-        <Route path="/contact" render={() => <Contact />} />
-      </Switch>
-    </Router>
+    <RouterProvider router = {router} />
   );
 }
 
---
+++ src/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div style={{ backgroundColor: 'white', minHeight: '100vh' }}>
+      {/* The above style sets the background to white and ensures the page takes up the full viewport height */}
+    </div>
+  )
+}
+
+export default Home;
