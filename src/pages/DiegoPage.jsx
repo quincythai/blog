0b6a536f3e8778e0e5559e815d690a83f40c49df
diego-padilla2027 <diego.padilla2027@berkeley.edu>
2023-10-25 15:44:01 -0700

    stuff added to diegoPage

--- src/pages/DiegoPage.jsx
+++ src/pages/DiegoPage.jsx
@@ -3,15 +3,25 @@ import '../styles/normalize.css';
 import '../styles/Page.css'
 import '../styles/DiegoPage.css'
 import { ReactMatrixAnimation } from 'react-matrix-animation';
+import { useEffect, useState } from 'react'
+import the_monkey from '/gilgamesh_monkey.jpg';
 
 const DiegoPage = () => {
+
+    const [activity, setActivity] = useState();
+    useEffect(() => {
+        fetch("https://www.boredapi.com/api/activity/")
+        .then(response => response.json())
+        .then(data => setActivity(data.activity))
+
+    }, []);
     return (
         <>
-            <div className = "Background">
             <TopBar />
 
-            <ReactMatrixAnimation className="matrix" />
-
+            <div className = "matrix">
+            <ReactMatrixAnimation />
+            </div>
 
 
 
@@ -21,77 +31,62 @@ const DiegoPage = () => {
                     HOW TO LOCK THE F*** IN!!!!!!!!!!
                 </h1>
 
-                <h4>
-                    <br />
-                    written by: Diego Padilla
-                </h4>
-
                 <p>
                     We posted up in Gilgamesh Times HQ!!! *weed smoking emoji*
+                    
                 </p>
                 <p>
-                    Locking in is all the rage with UC Berkeley students, but what does that really mean?
-                    In this page we are going over the 15 steps to completeley locking in through midterm season and finals.
+                    In just 10 short steps to completely locking in through midterm season and finals.
                 </p>
                 <ol>
-                    <li>
-                        <strong>Be inspirational:</strong> Start by defining your objectives and what you want to achieve with this task. Having a clear target will guide your efforts and help maintain focus.
+                    <li className = "lister">
+                        <strong>Be inspirational:</strong> If Daddy Denero isn't amazed at your grindset, I don't want to hear it
                     </li>
-                    <li>
-                        <strong>Choose a quiet and comfortable workspace to trap in:</strong> Find a location that is free from distractions and comfortable for long periods of work. Your workspace should support your concentration.
+                    <li className = "lister">
+                        <strong>Be Gilgamesh:</strong> Only big Gilgamesh energy can truly lock in
                     </li>
-                    <li>
-                        <strong>Gather all necessary materials and tools:</strong> Before you begin, make sure you have all the resources you need within easy reach. This minimizes the need to get up and disrupt your workflow.
+                    <li className = "lister">
+                        <strong>Be Gilgamesh again:</strong> Do it again. 
                     </li>
-                    <li>
-                        <strong>Turn off notifications on your devices:</strong> Silence or disable notifications on your phone, computer, and other gadgets to prevent constant interruptions. Consider using "Do Not Disturb" modes.
+                    <li className = "lister">
+                        <strong>Get rid of all distractions:</strong> Don't waist precious time on stupid things like "visiting your loved ones" or "having fun"
                     </li>
-                    <li>
-                        <strong>Put your phone on silent or in another room:</strong> Keep your phone out of sight and out of reach to minimize the temptation to check for messages or updates.
+                    <li className = "lister">
+                        <strong>Think about gilgamesh's legacy:</strong> And how it affects you
                     </li>
-                    <li>
-                        <strong>Close unnecessary tabs and applications on your computer:</strong> Limit digital distractions by closing unrelated tabs and applications. Focus on what's essential for your task.
+                    <li className = "lister">
+                        <strong>Imagine what you would say to gilgamesh if you could meet him:</strong> Do you think he would like me? lol
                     </li>
-                    <li>
-                        <strong>Organize your workspace for maximum efficiency:</strong> Keep your workspace tidy and uncluttered. Arrange items to minimize visual distractions and improve your workflow.
+                    <li className = "lister">
+                        <strong>Trent, do so much trent:</strong> 'nuff said
                     </li>
-                    <li>
-                        <strong>Use a timer to work in focused intervals (e.g., Pomodoro technique):</strong> Break your work into time intervals (e.g., 25 minutes) with short breaks in between. This helps maintain focus and prevents burnout.
+                    <li className = "lister">
+                        <strong>Gilgamesh:</strong> gilgamesh gilgamesh gilgamesh gilgamesh gilgamesh gilgamesh gilgamesh gilgamesh gilgamesh ğ̷̡̘̗̼͕̤͗͆̔ỉ̶̼͔̻̠̟̙̘̈́͋͗l̷̬̲̱͈̲̄͆̉͌̕g̷͔͇͍̝̠̪̳̃̂̈́͑á̵̞̰̙̦͐̈́m̶͍͑͂́͋̊͝e̵͉͚͍̿̒͑̆̉͘s̸̨̮̄́͛͜͝͝h̸̼̩̣͠
                     </li>
-                    <li>
-                        <strong>Eliminate visual distractions in your environment:</strong> Remove or cover up any items or objects that might divert your attention from the task at hand. Create a calming visual environment.
+                    <li className = "lister">
+                        <strong>Pilgramage to Mecca:</strong> mashallah
                     </li>
-                    <li>
-                        <strong>Take short breaks to recharge and refocus:</strong> Regular, short breaks give your mind a chance to recharge and help you maintain productivity and mental clarity.
+                    <li className = "lister">
+                        <strong>Get Raw:</strong> so unbelievably raw
                     </li>
-                    <li>
-                        <strong>Stay hydrated and have healthy snacks on hand:</strong> Proper nutrition and hydration support cognitive function and focus. Keep water and nutritious snacks nearby, and don't forget that purp drank.
-                    </li>
-                    <li>
-                        <strong>Listen to instrumental music or white noise if it helps you concentrate:</strong> Some people find that soft background music or white noise can improve concentration and block out distractions.
-                    </li>
-                    <li>
-                        <strong>Avoid multitasking; focus on one task at a time:</strong> Multitasking can diminish the quality of your work. Concentrate on a single task until it's completed or you reach a designated stopping point.
-                    </li>
-                    <li>
-                        <strong>Use task management tools or to-do lists to stay organized:</strong> Keep track of your tasks and prioritize them using digital tools or physical to-do lists. This helps you stay organized and on track.
-                    </li>
-                    <li>
-                        <strong>Reflect on your progress and adjust your approach as needed:</strong> Regularly assess your work and make adjustments as necessary. Adapting your approach based on your performance can lead to improved efficiency and productivity.
-                    </li>
+
                 </ol>
 
+                <h3>
+                    Gilgamesh activity to be absoluteley blessed up: {activity}
+                </h3>
+
+                <div className="author-article-container">
+                    <div className="author-info">
+                        <img src={the_monkey} alt="" />
+                        <p>Written by: d.p.</p>
+                </div>
+                </div>
+
+
             </div>
 
 
-
-            <p>Tags: React, Blogging, Web Development</p>
-            <p>
-                Share this post:
-
-            </p>
-
-            </div>
 
         </>
     )
