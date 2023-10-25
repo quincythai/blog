9f407faa8f529d02f44dbafe4b7ff7e70f870e72
Serrindipity <jon.quitoriano@gmail.com>
2023-10-25 16:10:09 -0700

    add code but not working

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,56 @@ import JQPage from './pages/JQPage'
 import AboutPage from './pages/about'
 import Footer from './components/Footer'
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Transition from './components/Transition';
+
 const App = () => {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element:<HomePage/>
-    },
-    {
-      path: '/DiegoPage',
-      element:<DiegoPage/>
-    },
-    {
-    path: '/QuincyPage',
-    element:<QuincyPage/>
-    },
-    {
-      path: '/JQPage',
-      element:<JQPage/>
-    },
-    {
-      path: '/about',
-      element: <AboutPage/>
-    },
-  ]);
-
-
-  
+  // const router = createBrowserRouter([
+  //   {
+  //     path: '/',
+  //     element:<HomePage/>
+  //   },
+  //   {
+  //     path: '/DiegoPage',
+  //     element:<DiegoPage/>
+  //   },
+  //   {
+  //   path: '/QuincyPage',
+  //   element:<QuincyPage/>
+  //   },
+  //   {
+  //     path: '/JQPage',
+  //     element:<JQPage/>
+  //   },
+  //   {
+  //     path: '/about',
+  //     element: <AboutPage/>
+  //   },
+  // ]);
+  // return <RouterProvider router={router} />;
   return (
     <>
-      <RouterProvider router = {router}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={
+          <Transition>
+            <HomePage />
+          </Transition>
+        } />
+        <Route path="DiegoPage" element={
+        <Transition>
+        <DiegoPage/>
+        </Transition>} />
+        <Route path="/QuincyPage" element={
+        <Transition>
+          <QuincyPage/>
+        </Transition>} />
+        <Route path="/JQPage" element={<Transition>
+          <JQPage/>
+          </Transition>} />
+        <Route path="/about" element={<AboutPage/>} />
+      </Routes>
+    </BrowserRouter>
       <Footer/>
     </>
   )
